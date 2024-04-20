51f4b1b6123a7882bd4305312bdaff86b4e79087
cordev-developer <jantoniocorralfiestas@gmail.com>
2024-04-20 09:28:29 +0200

    Remove AWS access key id and secret access key.

--- codigo-ejemplos/cognito-browser-imports-sdk-v3-vite/main.js
+++ codigo-ejemplos/cognito-browser-imports-sdk-v3-vite/main.js
@@ -29,8 +29,8 @@ imagenLogoJS.src = imgLogoJS;
 
 // Credenciales de developer con acceso a la API de Cognito
 const credentials = {
-  accessKeyId: "AKIA37XTGD7BSERGZB5T",
-  secretAccessKey: "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",
+  accessKeyId: "tu_access_key_id",
+  secretAccessKey: "tu_secret_access_key",
 }
 
 // Creamos el cliente de Cognito Identity Provider en la región "us-east-1" (N. Virginia) 
